826a2ddeca4f2a5013fb04ce0ac5a8e9d243ac25
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-08 15:05:13 +0700

    feat: use albums plugin, clean up onPreResponse

--- src/server.js
+++ src/server.js
@@ -1,14 +1,56 @@
 const Hapi = require('@hapi/hapi')
 
+// exception
+const { ClientError } = require('./exceptions')
+
+// plugin
+const albums = require('./api/albums')
+
+// service
+const AlbumsService = require('./services/postgres/AlbumsService')
+
+// utility
+const config = require('./utils/config')
+const { handleClientError, handleServerError } = require('./utils/responses')
+
+// validator
+const AlbumsValidator = require('./validator/albums')
+
 const init = async () => {
+    const albumsService = new AlbumsService()
+
     const server = Hapi.server({
-        port: 9000,
-        host: 'localhost',
+        port: config.app.port,
+        host: config.app.host,
         routes: {
             cors: {
                 origin: ['*']
             }
         }
+    })
+
+    await server.register([
+        {
+            plugin: albums,
+            options: {
+                service: albumsService,
+                validator: AlbumsValidator,
+            }
+        }
+    ])
+
+    server.ext('onPreResponse', (request, h) => {
+        const { response } = request
+
+        if (response instanceof Error) {
+            if (response instanceof ClientError) {
+                return handleClientError(response, h);
+            } else {
+                return handleServerError(response, h);
+            }
+        }
+
+        return h.continue
     })
     
     await server.start()
