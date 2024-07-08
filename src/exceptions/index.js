99bdd22f02d97fcb911ea15a41a5bcb8a7fedd56
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-08 15:00:48 +0700

    feat(exceptions): organize module export

---
+++ src/exceptions/index.js
@@ -0,0 +1,9 @@
+const ClientError = require('./ClientError')
+const NotFoundError = require('./NotFoundError')
+const InvariantError = require('./InvariantError')
+
+module.exports = { 
+    ClientError, 
+    NotFoundError,
+    InvariantError
+}
