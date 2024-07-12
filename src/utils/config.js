dbe4cb9c843b5122c4429d861db3ef9624183259
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-12 21:19:41 +0700

    feat: add security .env config

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,6 +5,14 @@ const config = {
         port: process.env.PORT,
         host: process.env.HOST,
     },
+    security: {
+        hash: {
+            saltRounds: parseInt(process.env.SALT_ROUNDS)
+        },
+        jwt: {
+
+        }
+    }
 }
 
 module.exports = config
