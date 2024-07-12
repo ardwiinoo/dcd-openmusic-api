670a97e6b71d2ff5a09cbceca30773a20174927c
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-12 21:17:24 +0700

    feat(import): add authentication & authorization

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -1,9 +1,13 @@
 const ClientError = require('./ClientError')
 const NotFoundError = require('./NotFoundError')
 const InvariantError = require('./InvariantError')
+const AuthenticationError = require('./AuthenticationError')
+const AuthorizationError = require('./AuthorizationError')
 
 module.exports = { 
     ClientError, 
     NotFoundError,
-    InvariantError
+    InvariantError,
+    AuthenticationError,
+    AuthorizationError
 }
