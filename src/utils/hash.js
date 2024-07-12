7aaa0ec6105435fd5b33f844aecd2cde089735b0
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-12 21:19:54 +0700

    feat: add password hash utility

---
+++ src/utils/hash.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcrypt')
+const config = require('./config')
+
+const hashPassword = async (password) => {
+    return await bcrypt.hash(password, config.security.hash.saltRounds) 
+}
+
+const comparePassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+}
+
+module.exports = {
+    hashPassword, 
+    comparePassword
+}
