646a82e3d6fd574dbb38c53ae7fd8ef9eb1397be
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-14 19:38:17 +0700

    feat: add verify & delete refresh token

--- src/services/postgres/AuthenticationsService.js
+++ src/services/postgres/AuthenticationsService.js
@@ -1,4 +1,5 @@
 const { Pool } = require("pg");
+const { InvariantError } = require("../../exceptions");
 
 class AuthenticationsService {
     constructor() {
@@ -8,6 +9,32 @@ class AuthenticationsService {
     async addRefreshToken(refreshToken) {
         const query = {
             text: 'INSERT INTO authentications (token) VALUES ($1)',
+            values: [refreshToken]
+        }
+
+        await this._pool.query(query)
+    }
+
+    async verifyRefreshToken(payload) {
+        const { refreshToken } = payload
+
+        const query = {
+            text: 'SELECT token FROM authentications WHERE token = $1',
+            values: [refreshToken]
+        }
+
+        const { rowCount } = await this._pool.query(query)
+        
+        if (!rowCount) {
+            throw new InvariantError('Refresh token tidak valid')
+        }
+    }
+
+    async deleteRefreshToken(payload) {
+        const { refreshToken } = payload
+
+        const query = {
+            text: 'DELETE FROM authentications WHERE token = $1',
             values: [refreshToken]
         }
 
