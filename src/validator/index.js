9c12b7f15d3e0b8d4fb9e566e2a9d9ee5f33a375
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-16 15:44:54 +0700

    feat: export collaborations validator

--- src/validator/index.js
+++ src/validator/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     AlbumsValidator: require('./albums'),
     AuthenticationsValidator: require('./authentications'),
+    CollaborationsValidator: require('./collaborations'),
     PlaylistsValidator: require('./playlists'),
     SongsValidator: require('./songs'),
     UsersValidator: require('./users')
