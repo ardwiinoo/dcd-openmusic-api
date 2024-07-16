70638f79ea117d8d3d7dcef0aa336689f8c9ffde
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-16 15:41:52 +0700

    feat: add collaborations handler

---
+++ src/api/collaborations/handler.js
@@ -0,0 +1,35 @@
+const autoBind = require('auto-bind')
+const { onSuccessResponse } = require('../../utils/responses')
+
+class CollaborationsHandler {
+    constructor(collaborationsService, playlistsService, validator) {
+        this._collaborationsService = collaborationsService,
+        this._playlistsService = playlistsService,
+        this._validator = validator
+
+        autoBind(this)
+    }
+
+    async postCollaborationHandler(request, h) {
+        const { id: userId } = request.auth.credentials
+
+        this._validator.validateCollaborationPayload(request.payload)
+        console.log(request.payload.playlistId)
+        await this._playlistsService.verifyPlaylistOwner(request.payload.playlistId, userId)
+        const collaborationId = await this._collaborationsService.addCollaborator(request.payload)
+
+        return onSuccessResponse(h, { data: { collaborationId }, statusCode: 201 })
+    }
+
+    async deleteCollaborationHandler(request, h) {
+        const { id: userId } = request.auth.credentials
+
+        this._validator.validateCollaborationPayload(request.payload)
+        await this._playlistsService.verifyPlaylistOwner(request.payload.playlistId, userId)
+        this._collaborationsService.deleteCollaboration(request.payload)
+
+        return onSuccessResponse(h, { message: 'Collaborator berhasil dihapus dari playlist' })
+    }
+}
+
+module.exports = CollaborationsHandler
