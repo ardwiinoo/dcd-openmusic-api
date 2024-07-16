124b2d4a17c9c683b27f4e461eabfac92fac66bf
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-16 15:43:37 +0700

    feat: export collaborations service

--- src/services/postgres/index.js
+++ src/services/postgres/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     AlbumsService: require('./AlbumsService'),
     AuthenticationsService: require('./AuthenticationsService'),
+    CollaborationsService: require('./CollaborationsService'),
     PlaylistsService: require('./PlaylistsService'),
     SongsService: require('./SongsService'),
     UsersService: require('./UsersService')
