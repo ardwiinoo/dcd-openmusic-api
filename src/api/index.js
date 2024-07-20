df62eec428b39a2aefff272731055725cafba2af
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-20 11:47:17 +0700

    feat: add exports plugin

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ module.exports = {
     albums: require('./albums'),
     authentications: require('./authentications'),
     collaborations: require('./collaborations'),
+    exports: require('./exports'),
     playlists: require('./playlists'),
     songs: require('./songs'),
     users: require('./users')
